6dc85b8e425af9ccde40436a63060e15c8073ae0
hyeoking <cksgurwkd12@naver.com>
2025-03-16 22:02:39 +0900

    [#10] ApiResponse 클래스 메시지 처리 개선
    - ApiResponse에서 직접 메시지 코드 처리하도록 개선
    - 메시지 처리 로직을 MessageUtil로 위임하여 중복 제거

---
+++ src/main/java/com/hyeok/ticketing/common/dto/ApiResponse.java
@@ -0,0 +1,37 @@
+package com.hyeok.ticketing.common.dto;
+
+
+import com.hyeok.ticketing.common.enums.ErrorCode;
+import com.hyeok.ticketing.common.enums.ResponseMessageCode;
+import com.hyeok.ticketing.common.util.MessageUtil;
+import lombok.Getter;
+
+@Getter
+public class ApiResponse<T> {
+
+    private final boolean success;
+    private final String message;
+    private final T data;
+
+    private ApiResponse(boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(true, message, data);
+    }
+
+    public static <T> ApiResponse<T> success(String message) {
+        return new ApiResponse<>(true, message, null);
+    }
+
+    public static <T> ApiResponse<T> fail(String message) {
+        return new ApiResponse<>(false, message, null);
+    }
+
+    public static <T> ApiResponse<T> fail(String message, T data) {
+        return new ApiResponse<>(false, message, data);
+    }
+}
